91f4341a210bd95ba93a785c5324a22ca69a0b24
donatma3 <donatma3@J09-04.uhk.cz>
2016-12-14 16:27:17 +0100

    Ukládání score do souboru csv

--- src/cz/uhk/fim/pro2/game/Game.java
+++ src/cz/uhk/fim/pro2/game/Game.java
@@ -1,13 +1,28 @@
 package cz.uhk.fim.pro2.game;
 
+import java.io.File;
+import java.io.IOException;
+
 import cz.uhk.fim.pro2.game.gui.GameScreen;
 import cz.uhk.fim.pro2.game.gui.HomeScreen;
 import cz.uhk.fim.pro2.game.gui.MainFrame;
 import cz.uhk.fim.pro2.game.gui.ScoreScreen;
 
 public class Game {
+	
+	public static final String SCORE_FILE = "scores.csv";
 
 	public static void main(String[] args) {
+		
+		File file = new File(SCORE_FILE);
+		if(!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("NEJDE VYTVORIT SOUBOR");
+			}
+		}
 		
 		MainFrame mainFrame = new MainFrame();
 		mainFrame.setScreen(new HomeScreen(mainFrame));
--- src/cz/uhk/fim/pro2/game/ScoreManager.java
+++ src/cz/uhk/fim/pro2/game/ScoreManager.java
@@ -1,6 +1,12 @@
 package cz.uhk.fim.pro2.game;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ScoreManager {
@@ -13,10 +19,48 @@ public class ScoreManager {
 	}
 	
 	public void addScore(int score) {
+		
+		List<Integer> scoreList = getAll();
+		scoreList.add(score);
+		
+		try {
+			FileWriter fileWriter = new FileWriter(Game.SCORE_FILE);
+			
+			for(int value : scoreList) {
+				fileWriter.append(String.valueOf(value));
+				fileWriter.append(";");
+				fileWriter.append(new Date().toGMTString());
+				fileWriter.append("\n");
+			}
+			
+			fileWriter.flush();
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Chyba pri zapisovani");
+		}
 		scoreList.add(score);
 	}
 	
 	public List<Integer> getAll() {
+		
+		List<Integer> scoreList = new ArrayList<>();
+		
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader(Game.SCORE_FILE);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			String line;
+			while((line = bufferedReader.readLine()) != null) {
+				String[] values = line.split(";");
+				scoreList.add(Integer.valueOf(values[0]));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Chyba pri nacitani");
+		}
+		
 		return scoreList;
 	}
 	
